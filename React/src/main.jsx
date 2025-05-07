6f04f06e68f9871ff0c0127d62b6aa8ca3732b89
Bhavya Soni <bhavya.soni@intimetec.com>
2025-05-07 10:55:45 +0530

    new type of routing

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import App from "./App.jsx";
+import "./index.css";
 // import store from './Redux/store.jsx'
-import { store } from './Redux-Toolkit/app/store.jsx'
-import { Provider } from 'react-redux'
+import { store } from "./Redux-Toolkit/app/store.jsx";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// we can also do this way
+// import { RouterProvider } from "react-router-dom";
+// import { createBrowserRouter } from 'react-router-dom'
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       }
+//     ]
+//   }
+// ])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <ChakraProvider theme={theme}>
+        {/* <RouterProvider router={router}> */}
         <App />
+        {/* </RouterProvider> */}
       </ChakraProvider>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
 
 // A React component is a JavaScript function that you can sprinkle with markup
-// Why do multiple JSX tags need to be wrapped? 
+// Why do multiple JSX tags need to be wrapped?
 // Provide key while rendering lists or converting array to li's
 // On responding to events always provide the reference to the function
 // don't call the value
 // Pure component is somewhat that returns the same value even called hundred times
-// Pure functions don’t mutate variables outside of the function’s scope 
+// Pure functions don’t mutate variables outside of the function’s scope
 // or objects that were created before the call—that makes them impure!
